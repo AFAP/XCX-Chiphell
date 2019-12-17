94c025cf1f447ba212d3e3bdefd59a709f8c0f69
AFAP <814237762@qq.com>
2019-12-17 22:04:02 +0800

    thread页面的展示修复
    采用正则处理图片的特殊链接

--- pages/thread/detail.js
+++ pages/thread/detail.js
@@ -2,6 +2,7 @@ const app = getApp()
 const NetUtils = require('../../utils/NetUtils.js');
 const Dom = require('../../libs/xmldom/dom-parser.js').DOMParser
 const Xpath = require('../../libs/xpath.js')
+const Html2Json = require('../../libs/html2json.js');
 
 Page({
   data: {
@@ -9,9 +10,10 @@ Page({
     articleId: '',
     totalPage: 1,
     richText: '',
-    commentNum: 0
+    commentNum: 0,
+    nodes: {}
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     let href = options.href;
     href = href.replace('article-', '');
     let articleId = href.split('-')[0];
@@ -21,26 +23,50 @@ Page({
     this.getDetail(1)
   },
   getDetail(tarPage) {
-    NetUtils.request(`/article-${this.data.articleId}-${tarPage}.html?&forcemobile=1`, 'get', {})
+    //https://www.chiphell.com/thread-2166781-1-1.html?mobile=2
+    NetUtils.request(`/thread-2166781-1-1.html`, 'get', {})
       .then((data) => {
         const rootDoc = new Dom().parseFromString(data);
-
         let floor1 = Xpath.select1("//*[@class = 'plc cl']", rootDoc);
+        // let floor1Doc = new Dom().parseFromString(ctNode.toString());
+        // let doc = Html2Json.html2json(floor1.toString());
+        // this.setData({
+        //   nodes:doc.child[0]
+        // })
+        // console.log(doc)
+        // let ctNode = app.getNode(floor1Doc, {
+        //   id: 'ct'
+        // })
+
         // let ctDoc = new Dom().parseFromString(ctNode.toString());
         // let contentNode = Xpath.select1("//*[@class = 'bm vw']", ctDoc)
         // let contentDoc = new Dom().parseFromString(contentNode.toString());
         // let tableNode = Xpath.select1("//*[@class = 'vwtb']", contentDoc)
 
-         
+
         let divHtml = floor1.toString();
-        divHtml = divHtml.replace(/<img/g, '<img style="width: 100%;"').replace(/&amp;nbsp;/g, '&nbsp;').replace(/src=\"/g,'src="https://static.chiphell.com/');
-          divHtml = this.data.richText + divHtml; 
-          this.setData({
-            richText: divHtml
-          }) 
+        divHtml = divHtml.replace(/&amp;/g, '&')
+        divHtml = divHtml.replace(/forum.php\?mod=image&aid=[^"]*"/g, function(value, index) {
+          const arr = value.split('&');
+          let aid = arr[1].split('=')[1];
+          let size = arr[2].split('=')[1];
+          aid = aid.padStart(9, '0');
+          let pp = aid.substring(0, 3) + '/' + aid.substring(3, 5) + '/' + aid.substring(5, 7) + '/' + aid.substring(7, 9);
+          let size2 = size.replace('x', '_')
+          const src = `https://static.chiphell.com/image/${pp}_${size2}.jpg`;
+          // console.log(value);
+          // console.log(aid);
+          // console.log(src);
+          return src + '" ';
+        })
+        divHtml = divHtml.replace(/<img/g, '<img style="width: 100%;"').replace(/&amp;nbsp;/g, '&nbsp;');
+        divHtml = this.data.richText + divHtml;
+        this.setData({
+          richText: divHtml
+        })
       })
   },
-  gotoComent: function (event) {
+  gotoComent: function(event) {
     wx.navigateTo({
       url: '/pages/article/comment?articleId=' + this.data.articleId,
     })
